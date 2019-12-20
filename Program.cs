6990c10f45e8596dae7d83a5e5ad974aafe2b1b3
Layomi Akinrinade <laakinri@microsoft.com>
2019-12-19 19:31:57 -0800

    Move input types to example dir

--- Program.cs
+++ Program.cs
@@ -25,22 +25,4 @@ namespace JsonConverterGenerator
             Console.WriteLine(generatedCode);
         }
     }
-
-    public class BasicPerson
-    {
-        public int Age { get; set; }
-        public string First { get; set; }
-        public string Last { get; set; }
-        public char MiddleInitial { get; set; }
-        public DateTimeOffset BirthDate { get; set; }
-        //public List<string> PhoneNumbers { get; set; }
-        //public BasicJsonAddress Address { get; set; }
-    }
-
-    public class BasicJsonAddress
-    {
-        public string Street { get; set; }
-        public string City { get; set; }
-        public int Zip { get; set; }
-    }
 }
---
+++ examples/InputTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace JsonConverterGenerator
+{
+    public class BasicPerson
+    {
+        public int Age { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public char MiddleInitial { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        //public List<string> PhoneNumbers { get; set; }
+        //public BasicJsonAddress Address { get; set; }
+    }
+
+    public class BasicJsonAddress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+    }
+}
